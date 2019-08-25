f0b015688caaaa07c9b1020d2c303e6cc5277f77
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-08-25 18:01:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,11 +47,7 @@ class TicTacToe
     index = input_to_index(input)
     if valid_move?(index)
       move(index, current_player)
-      if won?
-        break
-      else
-        display_board
-      end
+      display_board
     else
       puts "Invalid input."
       turn
